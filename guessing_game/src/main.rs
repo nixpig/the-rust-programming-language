86abba8e3f9f4956f1d2e893b5629704e3b88427
nixpig <143995476+nixpig@users.noreply.github.com>
2023-10-30 07:53:05 +0000

    feat: add guess limit to number guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,17 +3,28 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    let max_guesses = 10;
+    let max_range = 100;
+
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=max_range);
+    let mut counter = 0;
 
     loop {
         let mut guess = String::new();
+        println!("Guesses used: {counter}");
+        if counter == max_guesses - 1 {
+            println!("This is your last guess!!")
+        }
+
         println!("Please input your guess.");
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line.");
+
+        counter += 1;
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
@@ -29,6 +40,14 @@ fn main() {
                 println!("You win! 🥳");
                 break;
             }
+        }
+
+        if counter == max_guesses {
+            println!("You've used all of your guesses.");
+            println!("The correct answer was: {secret_number}");
+            println!("Better luck next time!");
+
+            break;
         }
     }
 }

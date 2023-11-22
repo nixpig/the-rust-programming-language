b773283878fc9c1085cc74ec9a57040d58c87f16
nixpig <143995476+nixpig@users.noreply.github.com>
2023-11-22 06:26:27 +0000

    chore: work on mutexes in concurrency chapter

--- Chapter 16 - Fearless Concurrency/mutexes/src/main.rs
+++ Chapter 16 - Fearless Concurrency/mutexes/src/main.rs
@@ -1,3 +1,43 @@
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+
+        println!("num: {}", num);
+
+        *num = 23;
+    }
+
+    println!("m: {:?}", m);
+
+    println!("----------");
+
+    let counter = Arc::new(Mutex::new(0));
+
+    let mut handles = vec![];
+
+    for i in 0..10 {
+        let counter_clone = Arc::clone(&counter);
+
+        let handle = thread::spawn(move || {
+            let mut c = counter_clone.lock().unwrap();
+            *c += 1;
+
+            println!("{i}: {c}");
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("counter: {}", counter.lock().unwrap());
 }
